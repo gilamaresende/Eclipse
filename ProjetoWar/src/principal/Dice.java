8ca6f3735414e4b64bcb7676acf1c7ca8fcd860b
Gilamr Fabiano <condelar@gmail.com>
2018-06-13 15:38:03 -0300

    Comentario

--- ProjetoWar/src/principal/Dice.java
+++ ProjetoWar/src/principal/Dice.java
@@ -4,10 +4,7 @@ import java.util.Random;
 
 public class Dice{
     Random dice;//dados
-    /**
-    *Dado usado na batalha
-    *
-    */
+    
     public Dice(){
         
         dice = new Random();
@@ -18,6 +15,12 @@ public class Dice{
     	
         return dice.nextInt(6) + 1; 
     }
+    /**
+     * 
+     * @param Recebe um inteiro que o valor maximo que pode ser retornado
+     * @return Retorna um inteiro entre 0 e o valor passado como parametro
+     */
+    
     //retorna um numeor aleatorio de 0 ate o valor pedido
     public int playsorte(int max) {
     	return dice.nextInt(max);
